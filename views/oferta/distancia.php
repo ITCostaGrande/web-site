41965bdf49e7166fedc49b5e957fea6a97b57c15
Manzanarez <mamanzasanchez@gmail.com>
2021-05-27 16:59:32 -0500

    Modificando la carpeta img

--- views/oferta/distancia.php
+++ views/oferta/distancia.php
@@ -1,19 +1,20 @@
-<div  class="Estilo4 centrado col-xs-12">
+<div class="Estilo4 centrado col-xs-12">
     <h2> Oferta Educativa en nivel Licenciatura vigente, Planes 2009-2010 por competencias profesionales:
     </h2>
-      
 
-        <br>
-            <h3><a1 href="left" >Ingeniería en Gestión Empresarial en Línea</a1></h3>
-            <img src="../img/lineahorizontal.png" alt="" width="680" height="29">
-            <a href="../archivos/FOLLETO IGE MODALIDAD EN LÍNEA 2020.pdf"><img src="../img/Portada IGE_Distancia.jpg"></a> 
-            <p><a href="ige.php">
-          </br>
-       <br>
-            <h4><a1 href="left" class="titulocarrera">Planes y Programas de Estudio</a1></h4>
-            </a></p>
-            <img src="../img/lineahorizontal.png" alt="" width="680" height="29"><!-- InstanceEndEditable -->    
- 
-   
- 
-     
+
+    <br>
+    <div class="centrado col-xs-6">
+        <h3>
+            <a1 href="left">Ingeniería en Gestión Empresarial en Línea</a1>
+        </h3>
+        <a href="../archivos/FOLLETO IGE MODALIDAD EN LÍNEA 2020.pdf"><img src="../img/muchas/Portada IGE_Distancia.jpg"></a>
+        <p><a href="ige.php">
+    </div>
+    <br>
+    <h4>
+        <a1 href="left" class="titulocarrera">Planes y Programas de Estudio</a1>
+    </h4>
+    </a></p>
+    <img src="../img/muchas/lineahorizontal.png" alt="" width="680" height="29"><!-- InstanceEndEditable -->
+</div>
