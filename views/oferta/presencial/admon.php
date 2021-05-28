b4256e3433b9cd58ad7ce2edd1be3d6ce223f0fd
Manzanarez <mamanzasanchez@gmail.com>
2021-05-28 13:01:24 -0500

    Se modifico el archivo admon.php

--- views/oferta/presencial/admon.php
+++ views/oferta/presencial/admon.php
@@ -1,1 +1,184 @@
-<h2>admon</h2>
+<div align="left" class="Estilo4 centrado col-xs-12">
+
+    <h2 href="left" class="titulocarrera">Licenciatura en Administración </h2>
+
+    <p class="info"> LADM-2010-234</p>
+
+    <img src="../img/logos/LogoLA.jpg" width="110" height="120">
+    <!---- <img src="img/logos/LogoSistemas.jpg" align="left" width="50" height="60"> --->
+    <hr class="divisor">
+</div>
+
+
+<div class="centrado">
+    <!---COLUMNAS DE ASIGNATURAS ---->
+
+    <!------- Tabla principal ------>
+    <table width="680" border="0" cellspacing="0" cellpadding="0" align="center">
+        <tr>
+            <!---COLUMNAS DE ASIGNATURAS ---->
+
+            <td width="250"> <a href="#" class="itlcuerpo"> Asignaturas:&nbsp;</a>
+                <br>
+                <a href="#" class="titulonota">Semestre 1: </a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAC-1035 Teoria General de la Administracion_OK_2016.pdf" target="_blank">Teoria General de la Administracion</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAV-1025 Informática para la Administración_OK_2016.pdf" target="_blank">Informática para la Administración</a> <br>
+                <a href="../Programas/Asignaturas Comunes/AC007 Taller de Etica.pdf" target="_blank">Taller de ética</a> <br>
+                <a href="../Programas/Asignaturas Comunes/AC006 Fundamentos de Investigacion.pdf" target="_blank">Fundamentos de Investigación</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAD-1027 Matematicas Aplicadas a la Administracion_OK_2016.pdf" target="_blank">Matematicas Aplicadas a la Administracion</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAD-1006 Contabilidad General_OK_2016.pdf" target="_blank">Contabilidad General</a> <br>
+                <!------------------- semestre 2 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 2: </a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAF-1019 Funcion Administrativa I_OK_2016.pdf" target="_blank">Funcion Administrativa I</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAD-1016 Estadística para la Administración I_OK_2016.pdf" target="_blank">Estadística para la Administración I</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAF-1010 Derecho Laboral Seguridad Social_OK_2016.pdf" target="_blank">Derecho Laboral y Seguridad Social</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAC-1004 Comunicación Corporativa_OK_2016.pdf" target="_blank">Comunicación Corporativa</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAC-1034 Taller de Desarrollo Humano_OK_2016.pdf" target="_blank">Taller de Desarrollo Humano</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAD-1008 Costos de manufactura_OK_2016.pdf" target="_blank">Costos de Manufactura</a> <br>
+                <!------------------- semestre 3 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 3: </a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAD-1020 Funcion Administrativa II_OK_2016.pdf" target="_blank">Funcion Administrativa II</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAD-1017 Estadística para la administración II_OK_2016.pdf" target="_blank">Estadística para la administración II</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAD-1009 Derecho Empresarial_OK_2016.pdf" target="_blank">Derecho Empresarial</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAD-1003 Comportamiento Organizacional_OK_2016.pdf" target="_blank">Comportamiento Organizacional</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAC-1013 Dinamica Social_OK_2016.pdf" target="_blank">Dinamica Social</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAD-1007 Contabilidad Gerencial_OK_2016.pdf" target="_blank">Contabilidad Gerencial</a> <br>
+                <!------------------- semestre 4 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 4: </a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAD-1023 Gestion Estrategica del Capital Humano I_OK_2016.pdf" target="_blank">Gestion Estrategica del Capital Humano I</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAD-1031 Procesos Estructurales_OK_2016.pdf" target="_blank">Procesos Estructurales</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAD-1028 Metodos Cuantitativos para la Administración_OK_2016.pdf" target="_blank">Metodos Cuantitativos para la Administración</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAF-1021 Fundamentos de Mercadotecnia_OK_2016.pdf" target="_blank">Fundamentos de Mercadotecnia</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAD-1014 Economia Empresarial_OK_2016.pdf" target="_blank"> Economia Empresarial</a> <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE079 Matematicas Financieras.pdf" target="_blank">Matematicas Financieras</a> <br>
+                <!------------------- semestre 5 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 5: </a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAD-1024 Gestión Estratégica Capital Humano II_OK_2016.pdf" target="_blank">Gestión Estratégica Capital Humano II</a> <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE070 Derecho Fiscal.pdf" target="_blank">Derecho Fiscal</a> <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE080 Mezcla de Mercadotecnia.pdf" target="_blank">Mezcla de Mercadotecnia</a> <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE077 Macroeconomia.pdf" target="_blank">Macroeconomia</a> <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE068 Administracion Financiera I.pdf" target="_blank">Administracion Financiera I</a> <br>
+                <a href="../Programas/Asignaturas Comunes/AC008 Desarrollo Sustentable.pdf" target="_blank">Desarrollo Sustentable</a> <br>
+                <!------------------- semestre 6 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 6: </a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAM-1022 Gestión de la  Retribución_OK_2016.pdf" target="_blank">Gestión de la Retribución</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAF-1032 Producción_OK_2016.pdf" target="_blank">Producción</a> <br>
+                <a href="../Programas/Asignaturas Comunes/AC009 Taller de Investigacion I.pdf" target="_blank">Taller de Investigacion I</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAD-1033 Sistemas de Información de Mercadotecnia_OK_2016.pdf" target="_blank">Sistemas de Información de Mercadotecnia</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAA-1026 Innovación y Emprendedurismo_OK_2016.pdf" target="_blank">Innovación y Emprendedurismo</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAD-1002 Administración Financiera II_OK_2016.pdf" target="_blank">Administración Financiera II</a> <br>
+                <!------------------- semestre 7 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 7: </a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAB-1029 Plan de Negocios_OK_2016.pdf" target="_blank">Plan de Negocios</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAC-1030 Procesos de Direccion_OK_2016.pdf" target="_blank">Procesos de Direccion</a> <br>
+                <a href="../Programas/Asignaturas Comunes/AC010 Taller de Investigacion II.pdf" target="_blank">Taller de Investigacion II</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAD-1001 Administracion de la Calidad_OK_2016.pdf" target="_blank">Administracion de la Calidad</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAC-1015 Economia Internacional_OK_2016.pdf" target="_blank">Economia Internacional</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAD-1012 Diagnóstico y Evaluación Empresarial_OK_2016.pdf" target="_blank">Diagnóstico y Evaluación Empresarial</a> <br>
+                <!------------------- semestre 8 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 8: </a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAC-1005 Consultoria  Empresarial_OK_2016.pdf" target="_blank">Consultoria Empresarial</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAD-1018 Formulacion y Evaluacion de Proyectos_OK_2016.pdf" target="_blank">Formulacion y Evaluacion de Proyectos</a> <br>
+                <a href="../Programas/Lic en Administracion LADM - 2010 -234/Asignaturas/LAD-1011 Desarrollo Organizacional" target="_blank">Desarrollo Organizacional</a>
+
+            </td>
+
+            <!------------------- FINALIZA LA TABLA DE ASIGNATURAS ---------------->
+
+            <!---COLUMNAS DE PERFILIL Y RETICULAS ---->
+            <td width="415" valign="top" align="justify">
+                <div class="b_lateral">
+                    <p class="itlcuerpo"><a href="../Programas/Lic en Administracion LADM - 2010 -234/Perfil-Objetivo Licenciatura en Administracion.pdf" target="_blank">Objetivo General</a></p>
+                    <br>
+                    <span class="nota">Formar profesionales de la administración comprometidos con las demandas y oportunidades del entorno, con una visión estratégica, humanista y global, que actúen como agentes de cambio, a través del diseño, innovación y dirección en organizaciones.</span>
+                    <br><br>
+                    <p class="itlcuerpo"> <a href="#" target="_blank">MISIÓN </a></p>
+                    <span class="nota"> Formar profesionales de la administración, capaces de tomar decisiones, asumir riesgos, desarrollar innovaciones, interpretar tendencias económicas, sociales, políticas, culturales, ambientales y tecnológicas, generando su inserción competitiva en el entorno global con sustentabilidad ética y responsabilidad social.</span>
+                    <br><br>
+                    <p class="itlcuerpo"> <a href="#" target="_blank"> VISIÓN </a></p>
+
+                    <span class="nota">El programa de Licenciatura en Administración se constituirá en el largo plazo como un sistema académico certificado y consolidado en la Costa Grande de Guerrero, caracterizado por contar con diseño curricular flexible, basado en el modelo educativo vigente.</span>
+                    <br><br>
+                    <p class="itlcuerpo"> <a href="">PERFIL DE INGRESO</a> </p>
+                    <span class="nota"> Deberá contar con los conocimientos mínimos de las áreas de humanidades, ciencias sociales y naturales, además de demostrar su capacidad de razonamiento verbal y matemático para ser capaz de interpretar lecturas, entender y utilizar analogías y demostrar la amplitud de su vocabulario. Debe ser una persona con independencia de criterio, capaz de formarse un juicio propio, a partir de un análisis de datos, así como de razonar de manera eficiente bajo presión de tiempo.
+                    </span>
+                    <br><br>
+                    <p class="itlcuerpo"> <a href="../Programas/Lic en Administracion LADM - 2010 -234/Perfil-Objetivo Licenciatura en Administracion.pdf" target="_blank">Perfil de Egreso</a></p>
+
+                    <br>
+                    <span class="nota">
+                        1. Integra los procesos gerenciales, de administración, de innovación y las estrategias para alcanzar la productividad y competitividad de las organizaciones.
+                        <br>
+                        2. Adapta las etapas de los procesos a las nuevas tendencias y enfoques de la administración, para la optimización de los recursos y el manejo de los cambios organizacionales, de acuerdo a las
+                        necesidades del entorno.
+                        <br>
+                        3. Desarrolla habilidades directivas basadas en la ética y la responsabilidad social, que le permitan integrar y coordinar equipos interdisciplinarios y multidisciplinarios para el crecimiento de la organización.
+                        <br>
+                        4. Crea y desarrolla proyectos sustentables aplicando métodos de investigación de vanguardia, con un enfoque estratégico, multicultural y humanista.
+                        <br>
+                        5. Dirige la organización hacia la consecución de sus objetivos, a través de la coordinación de
+                        esfuerzos y el desarrollo creativo.
+                        <br>
+                        6. Diseña organizaciones que contribuyan a la transformación económica y social identificando las
+                        oportunidades de negocios.
+                        <br>
+                        7. Interpreta y aplica el marco legal vigente nacional e internacional, para dar certeza jurídica a las organizaciones.
+                        <br>
+                        8. Interpreta información financiera y económica para la toma de decisiones en las organizaciones.
+                        <br>
+                        9. Desarrolla el capital humano a través de la utilización de técnicas y herramientas gerenciales para la toma de decisiones.
+                        <br>
+                        10. Implementa y administra sistemas de gestión de calidad para orientarlos a la mejora continua, con la finalidad de lograr la eficacia y eficiencia de la organización.
+                        <br>
+                        11. Aplica las tecnologías de la información y comunicación en el diseño de estrategias que optimicen el trabajo y desarrollo de las organizaciones.
+                        <br>
+                        12. Posee una visión multidisciplinaria para generar acciones ante escenarios de contingencia
+                        salvaguardando la integridad humana.
+                        <br>
+                        13. Diseña estrategias de mercadotecnia basadas en el análisis de la información interna y del entorno global, para asegurar el éxito de la comercialización de bienes y servicios de las organizaciones.
+                    </span>
+                    <br><br>
+                    <p class="itlcuerpo"><a href="">CAMPO DE TRABAJO:</a> </p>
+                    <span class="nota">
+                        • Empresas públicas: Municipales, estatales y federales. <br>
+                        • Empresas privadas: Industriales, comerciales y servicios. <br>
+                        • Organizaciones de la sociedad civil. <br>
+                        • Generadoras de su propia empresa <br>
+                    </span>
+                    <br><br>
+                    <p class="itlcuerpo"> <a href="">ÁMBITO</a> </p>
+
+                    <span class="nota"> Administración, Producción, Finanzas, Recursos Humanos, Mercadotecnia, Calidad.</span>
+                    <br><br>
+                    <p class="itlcuerpo"><a href="">POLÍTICAS</a> </p>
+                    <span class="nota">
+                        • Los licenciados en administración deberán: <br>
+                        • Tener una participación activa, crítica propositiva. <br>
+                        • Desarrollar actitud de servicio con la comunidad. <br>
+                        • Aplicar aptitudes y habilidades de organización. <br>
+                        • Establecer destreza para intervenir con enfoque interdisciplinario. <br>
+                        • Generar habilidad de comunicación. <br>
+                        • Ejercer liderazgo en su ámbito de aplicación laboral <br>
+                        • Constituirse como gestores de cambio en el desarrollo económico local, regional, nacional e internacional. <br>
+                        • Aplicar conocimientos, habilidades y actitudes proactivas para la toma de decisiones. <br>
+                    </span>
+                </div>
+                <br><br>
+                <p class="itlcuerpo"> <a href="../Programas/Lic en Administracion LADM - 2010 -234/Reticula Admon.pdf" target="_blank">Retícula</a></p>
+                <br><br>
+                <p class="itlcuerpo"> <a href="../Programas/Lic en Administracion LADM - 2010 -234/Reticula Administracion esp.pdf" target="_blank">ESPECIALIDAD</a></p>
+                <br>
+                <span class="nota">
+                    Mercadotecnia (LADE-MER-2018-01)</span>
+
+            </td>
+        </tr>
+    </table>
+</div>
