aa762341fa902a5585628313b52d4115acbde3e7
Manzanarez <mamanzasanchez@gmail.com>
2021-05-28 00:53:25 -0500

    Ultimo modificado arquitectura.php

--- views/oferta/presencial.php
+++ views/oferta/presencial.php
@@ -6,13 +6,13 @@
 
         <div class="menu_carreras">
             <ul>
-                <li><a href=""><h4>ARQUITECTURA</h4></a></li>
-                <li><a href=""><h4>CONTADOR PÚBLICO</h4></a></li>
-                <li><a href=""><h4>LICENCIATURA EN ADMINISTRACIÓN</h4></a></li>
-                <li><a href=""><h4>INGENIERÍA EN TIC'S</h4></a></li>
-                <li><a href=""><h4>INGENIERÍA EN GESTIÓN EMPRESARIAL</h4></a></li>
-                <li><a href=""><h4>INGENIERÍA EN SISTEMAS COMPUTACIONALES</h4></a></li>
-                <li><a href=""><h4>INGENIERÍA ELECTROMECÁNICA</h4></a></li>
+                <li><a href="/oferta/arquitectura"><h4>ARQUITECTURA</h4></a></li>
+                <li><a href="/oferta/cp"><h4>CONTADOR PÚBLICO</h4></a></li>
+                <li><a href="/oferta/admon"><h4>LICENCIATURA EN ADMINISTRACIÓN</h4></a></li>
+                <li><a href="/oferta/tics"><h4>INGENIERÍA EN TIC'S</h4></a></li>
+                <li><a href="/oferta/isc"><h4>INGENIERÍA EN GESTIÓN EMPRESARIAL</h4></a></li>
+                <li><a href="/oferta/electromecanica"><h4>INGENIERÍA EN SISTEMAS COMPUTACIONALES</h4></a></li>
+                <li><a href="/oferta/ige"><h4>INGENIERÍA ELECTROMECÁNICA</h4></a></li>
             </ul>
         </div>
     </div>
--- views/oferta/presencial/arquitectura.php
+++ views/oferta/presencial/arquitectura.php
@@ -1,1 +1,208 @@
-<h2>Arqui</h2>
+<div align="left" class="Estilo4 centrado col-xs-12">
+    <h2 href="titulocarrera" align="center">Arquitectura</h2>
+    <img src="../img/logos/LogoArquitectura.jpg" width="110" height="120" align="center">
+    <!---- <img src="img/logos/LogoSistemas.jpg" align="left" width="50" height="60"> --->
+    <hr class="divisor">
+
+</div>
+<div class=" centrado">
+    <!------- Tabla principal ------>
+    <table width="680" border="0" cellspacing="0" cellpadding="0" align="center">
+        <tr>
+            <!---COLUMNAS DE ASIGNATURAS ---->
+            <td width="250"> <a href="#" class="itlcuerpo"> Asignaturas:&nbsp;</a>
+                <!---  <div class="cintillo"></div> --->
+                <!-------------Semestre 1 ----------------------------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 1: </a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Fundamentos teóricos del diseño I.pdf" target="_blank">Fundamentos Teóricos del Diseño I</a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC006 Fundamentos de Investigacion.pdf" target="_blank">Fundamentos de Investigación</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Matemáticas Aplicadas a la Arquitectura.pdf" target="_blank">Matemáticas Aplicadas a la Arquitectura</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Geometria Descriptiva I.pdf" target="_blank">Geometría Descriptiva I</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Análisis crítico de la Arquitectura y el Arte I.pdf" target="_blank">Análisis Crítico de la Arquitectura y el Arte I</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Taller de Expresión Plástica.pdf" target="_blank">Taller de Expresión Plástica</a>
+                <br>
+
+                <!------------------- semestre 2 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 2: </a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Fundamentos teóricos del diseño II.pdf" target="_blank">Fundamentos Teóricos del Diseño II</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Metodología para el Diseño.pdf" target="_blank">Metodología para el Diseño</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Análisis Proyectual.pdf" target="_blank">Análisis Proyectual</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Geometría Descriptiva II.pdf" target="_blank">Geometría Descriptiva II</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Análisis Crítico de la Arquitectura y el Arte II.pdf" target="_blank">Análisis Crítico de la Arquitectura y el Arte II</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Taller de Lenguaje Arquitectónico I.pdf" target="_blank">Taller de Lenguaje Arquitectónico I</a>
+                <br>
+                <!------------------- semestre 3 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 3: </a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Taller de Diseño I.pdf" target="_blank">Taller de Diseño I</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Propiedades y Comportamiento de los Materiales.pdf" target="_blank">Propiedades y Comportamiento de los Materiales</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Estructuras I.pdf" target="_blank">Estructuras I</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Topografía.pdf" target="_blank">Topografía</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Análisis crítico de la Arquitectura y el Arte III.pdf" target="_blank">Análisis Crítico de la Arquitectura y el Arte III</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Taller de Lenguaje Arquitectónico II.pdf" target="_blank">Taller de Lenguaje Arquitectónico II</a>
+                <br>
+                <!------------------- semestre 4 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 4: </a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Taller de Diseño II.pdf" target="_blank">Taller de Diseño II</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Taller de Construcción I.pdf" target="_blank">Taller de Construcción I</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Estructuras II.pdf" target="_blank">Estructuras II</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Pensamiento Arquitectónico Contemporaneo.pdf" target="_blank">Pensamiento Arquitectónico Contemporáneo</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Análisis crítico de la Arquitectura y el Arte IV.pdf" target="_blank">Análisis Crítico de la Arquitectura y el Arte IV</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Instalaciones I.pdf" target="_blank">Instalaciones I</a>
+                <br>
+
+                <!------------------- semestre 5 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 5: </a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Taller de Diseño III.pdf" target="_blank">Taller de Diseño
+                    III</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Taller de Construcción II.pdf" target="_blank">Taller de Construcción II</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Estructuras de concreto.pdf" target="_blank">Estructuras de Concreto</a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC008 Desarrollo Sustentable.pdf" target="_blank">Desarrollo Sustentable</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Estética.pdf" target="_blank">Estética</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Instalaciones II.pdf" target="_blank">Instalaciones II</a>
+                <br>
+                <!------------------- semestre 6 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 6: </a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Taller de Diseño IV.pdf" target="_blank">Taller de Diseño IV</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Estructuras de Acero.pdf" target="_blank">Estructuras de Acero</a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC009 Taller de Investigacion I.pdf" target="_blank">Taller de Investigación I</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Urbanismo I.pdf" target="_blank">Urbanismo I</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Administración de la Construcción I.pdf" target="_blank">Administración
+                    de la Construcción I</a>
+                <br>
+
+                <!------------------- semestre 7 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 7: </a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Taller de Diseño V.pdf" target="_blank">Taller de Diseño V</a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC007 Taller de Etica.pdf" target="_blank">Taller de Ética</a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC010 Taller de Investigacion II.pdf" target="_blank">Taller de Investigación II</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Urbanismo II.pdf" target="_blank">Urbanismo II</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Administración de la Construcción II.pdf" target="_blank">Administración de la Construcción II</a>
+                <br>
+
+                <!------------------- semestre 8 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 8: </a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Taller de Diseño VI.pdf" target="_blank">Taller de Diseño VI</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Gestión Urbanística.pdf" target="_blank">Gestión
+                    Urbanística</a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Administración de Empresas Constructoras I.pdf" target="_blank">Administración de Empresas Constructoras I</a>
+                <br>
+                <!---------- semestre 9 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 9: </a>
+                <br>
+                <a href="../Programas/Arquitectura ARQU - 2010 - 204/Asignaturas/Administración de Empresas Constructoras II.pdf" target="_blank">Administración
+                    de Empresas Constructoras II</a>
+
+            </td>
+            <!------------------- FINALIZA LA TABLA DE ASIGNATURAS ---------------->
+            <td width="10" valign="top"><img src="img/lineavertical.png" alt="" width="10" height="1000"> </td>
+
+            <!---COLUMNAS DE PERFILIL Y RETICULAS ---->
+            <td width="415" valign="top" align="justify">
+                <div class="b_lateral">
+                    <p class="itlcuerpo"><a href="../Programas/Arquitectura ARQU - 2010 - 204/Perfil-Objetivo Arquitectura.pdf" target="_blank">Objetivo General&nbsp;</a></p>
+                    <br>
+                    <span class="nota">Formar profesionistas líderes con excelencia académica y humanística, capaces de planear diseñar, gestionar y construir el hábitat humano con alto desempeño y vocación de servicio a la sociedad.</span> <br>
+                    <br><br>
+                    <p class="itlcuerpo"> <a href="../Programas/Arquitectura ARQU - 2010 - 204/Perfil-Objetivo Arquitectura.pdf" target="_blank">Perfil de egreso&nbsp;</a></p>
+
+                    <br>
+                    <span class="nota">
+                        1. Diseña proyectos urbano-arquitectónicos de manera integral, para crear el hábitat humano,
+                        respetando los marcos normativos del entorno y contexto, aplicando criterios de diseño universal.
+                        <br>
+                        2. Selecciona, aplica y evalúa tecnologías, materiales y sistema constructivos, para la ejecución de obras, considerando su ciclo de vida.
+                        <br>
+                        3. Planea, gestiona y opera desarrollos urbanos, con visión sostenible para elevar la calidad de vida.
+                        <br>
+                        4. Selecciona y diseña estructuras e instalaciones viables y sustentables, para el desarrollo de
+                        proyectos urbanos arquitectónicos.
+                        <br>
+                        5. Administra el proceso constructivo de las obras urbano-arquitectónicas para la optimización de los recursos, con base al marco normativo vigente.
+                        <br>
+                        6. Asesora en la valoración y conservación del patrimonio, re-arquitectura, proyectos de inversión
+                        inmobiliaria y legislación urbana para la toma de decisiones de los sectores público y privado.
+                        <br>
+                        7. Dirige organismos y grupos inter y multidisciplinarios para la integración de proyectos urbanoarquitectónicos
+                        <br>
+                        8. Actúa de manera ética, en su ejercicio profesional para el bienestar de la sociedad y su entorno.
+                    </span>
+                    </div>
+                    <br><br>
+
+                    <p class="itlcuerpo titulonota"> <a href="../Programas/Arquitectura ARQU - 2010 - 204/Reticula Arquitectura.pdf" target="_blank"><span class="span_pdf">Retícula</span> </a> </p>
+
+                
+
+            </td>
+        </tr>
+
+        <!--    <tr>  
+          <td colspan="2"><img src="img/lineahorizontal.png" alt="" width="680" height="29"></td>
+        </tr>
+        <tr>
+          <td width="267">&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>   --->
+    </table>
+    <!--- AQUá ACABA LOS BOLETINES ------>
+
+
+    <!------- Finaliza Tabla principal ------>
+    <!-- InstanceEndEditable -->
+
+
+
+</div>
+</td>
