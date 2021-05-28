1f7d02e77620141ca78aa31fe38e372b682968de
Manzanarez <mamanzasanchez@gmail.com>
2021-05-28 17:15:02 -0500

    Se modifico el archivo tics.php

--- views/oferta/presencial/tics.php
+++ views/oferta/presencial/tics.php
@@ -1,1 +1,220 @@
-<h2>tics</h2>
+<div align="left" class="Estilo4 centrado col-xs-12">
+
+    <h2>Ingeniería en Tecnologías de la Información y Comunicaciones</h2>
+
+    <img src="../img/logos/LogoTICs.jpg" align="center" width="110" height="120">
+    <hr class="divisor">
+</div>
+
+<!------- Tabla principal ------>
+<div class="col-xs-12 centrado">
+    <div class="centrado cuerpo">
+        <!---COLUMNAS DE ASIGNATURAS ---->
+        <div class="menu_materias">
+            <a href="#" class="itlcuerpo"> Asignaturas:&nbsp;</a>
+            <!---  <div class="cintillo"></div> --->
+            <!-------------Semestre 1 ----------------------------------->
+            <a href="#" class="titulonota">Semestre 1: </a>
+            <br>
+            <a href="../Programas/Asignaturas Comunes/AC001 Calculo Diferencial.pdf" target="_blank">Cálculo Diferencial</a>
+            <br>
+            <a href="../Programas/Asignaturas Equivalentes/AE032 Fundamentos de Programacion.pdf" target="_blank">Fundamentos de Programación</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Matematicas Discretas I.pdf" target="_blank">Matemáticas Discretas I</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Introduccion a las TICs.pdf" target="_blank">Introducción a las TICs</a>
+            <br>
+            <a href="../Programas/Asignaturas Comunes/AC007 Taller de Etica.pdf" target="_blank">Taller de Ética</a>
+            <br>
+            <a href="../Programas/Asignaturas Comunes/AC006 Fundamentos de Investigacion.pdf" target="_blank">Fundamentos de Investigación</a>
+            <br>
+
+            <!------------------- semestre 2 ---------------->
+            <br>
+            <a href="#" class="titulonota">Semestre 2: </a>
+            <br>
+            <a href="../Programas/Asignaturas Comunes/AC002 Calculo Integral.pdf" target="_blank">Cálculo Integral</a>
+            <br>
+            <a href="../Programas/Asignaturas Equivalentes/AE086 Programacion Orientada a Objetos.pdf" target="_blank">Programación Orientada a Objetos</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Matematicas Discretas II.pdf" target="_blank">Matemáticas Discretas II</a>
+            <br>
+            <a href="../Programas/Asignaturas Comunes/AC003 Algebra Lineal.pdf" target="_blank">Álgebra Lineal</a>
+            <br>
+            <a href="../Programas/Asignaturas Equivalentes/AE052 Probabilidad y Estadistica.pdf" target="_blank">Probabilidad y Estadística</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Contabilidad y Costos.pdf" target="_blank">Contabilidad y Costos</a>
+            <br>
+            <!------------------- semestre 3 ---------------->
+            <br>
+            <a href="#" class="titulonota">Semestre 3: </a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Estructuras y Organización de Datos.pdf" target="_blank">Estructuras y Organización de Datos</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Matematicas para la Toma de Decisiones.pdf" target="_blank">Matemáticas para la Toma de Decisiones</a>
+            <br>
+            <a href="../Programas/Asignaturas Equivalentes/AE031 Fundamentos de Base de Datos.pdf" target="_blank">Fundamentos de Base de Datos</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Electricidad y Magnetismo.pdf" target="_blank">Electricidad y Magnetismo</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Administración Gerencial.pdf" target="_blank">Administración Gerencial</a>
+            <br>
+
+            <!------------------- semestre 4 ---------------->
+            <br>
+            <a href="#" class="titulonota">Semestre 4: </a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Matemáticas Aplicadas a Comunicaciones.pdf" target="_blank">Matemáticas Aplicadas a Comunicaciones</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Programación II.pdf" target="_blank">Programación II</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Fundamentos de Redes.pdf" target="_blank">Fundamentos de Redes</a>
+            <br>
+            <a href="../Programas/Asignaturas Equivalentes/AE063 Taller de Base de Datos.pdf" target="_blank">Taller de Base de Datos</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Circuitos Eléctricos y Electrónicos.pdf" target="_blank">Circuitos Eléctricos y Electrónicos</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Ingeniería de Software.pdf" target="_blank">Ingeniería de Software</a>
+            <br>
+
+            <!------------------- semestre 5 ---------------->
+            <br>
+            <a href="#" class="titulonota">Semestre 5: </a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Análisis de Señales y Sistemas de Comunicación.pdf" target="_blank">Análisis de Señales y Sistemas de Comunicación</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Administración de Proyectos.pdf" target="_blank">Administración de Proyectos</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Redes de Computadoras.pdf" target="_blank">Redes de Computadoras</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Bases de Datos Distribuidas.pdf" target="_blank">Bases de Datos Distribuidas</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Arquitectura de Computadoras.pdf" target="_blank">Arquitectura de Computadoras</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Taller de Ingeniería de Software.pdf" target="_blank">Taller de Ingeniería de Software</a>
+            <br>
+            <!------------------- semestre 6 ---------------->
+            <br>
+            <a href="#" class="titulonota">Semestre 6: </a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Telecomunicaciones.pdf" target="_blank">Telecomunicaciones</a>
+            <br>
+            <a href="../Programas/Asignaturas Equivalentes/AE055 Programacion Web.pdf" target="_blank">Programación Web</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Desarrollo de Emprendedores.pdf" target="_blank">Desarrollo de Emprendedores</a>
+            <br>
+            <a href="../Programas/Asignaturas Equivalentes/AE061 Sistemas Operativos I.pdf" target="_blank">Sistemas Operativos I</a>
+            <br>
+            <a href="../Programas/Asignaturas Comunes/AC008 Desarrollo Sustentable.pdf" target="_blank">Desarrollo Sustentable</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Tecnologías Inalámbricas.pdf" target="_blank">Tecnologías Inalámbricas</a>
+            <br>
+
+            <!------------------- semestre 7 ---------------->
+            <br>
+            <a href="#" class="titulonota">Semestre 7: </a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Redes Emergentes.pdf" target="_blank">Redes Emergentes</a>
+            <br>
+            <a href="../Programas/Asignaturas Equivalentes/AE011 Desarrollo Aplicaciones Dispositivos Moviles.pdf" target="_blank">Desarrollo de Aplicaciones para Dispositivos Móviles</a>
+            <br>
+            <a href="../Programas/Asignaturas Comunes/AC009 Taller de Investigacion I.pdf" target="_blank">Taller de Investigación I</a>
+            <br>
+            <a href="../Programas/Asignaturas Equivalentes/AE062 Sistemas Operativos II.pdf" target="_blank">Sistemas Operativos II</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Negocios Electrónicos I.pdf" target="_blank">Negocios Electrónicos I</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Interacción Humano Computadora.pdf" target="_blank">Interacción Humano Computadora</a>
+            <br>
+
+            <!------------------- semestre 8 ---------------->
+            <br>
+            <a href="#" class="titulonota">Semestre 8: </a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Administración y Seguridad de Redes.pdf" target="_blank">Administración y Seguridad de Redes</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Auditoría en Tecnologías de la Información.pdf" target="_blank">Auditoría en Tecnologías de la Información</a>
+            <br>
+            <a href="../Programas/Asignaturas Comunes/AC010 Taller de Investigacion II.pdf" target="_blank">Taller de Investigacion II</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Ingeniería del Conocimiento.pdf" target="_blank">Ingeniería del Conocimiento</a>
+            <br>
+            <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Asignaturas/Negocios Electrónicos II.pdf" target="_blank">Negocios Electrónicos II</a>
+
+
+        </div>
+        <!------------------- FINALIZA LA TABLA DE ASIGNATURAS ---------------->
+
+        <!---COLUMNAS DE PERFILIL Y RETICULAS ---->
+        <div class="descripcion_carrera">
+            <div class="b_lateral">
+                <p class="itlcuerpo"><a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Perfil-Objetivo Ingenieria en Tecnologias de la Informacion y Comunicaciones.pdf" target="_blank">Objetivo General</a></p>
+                <br>
+                <span class="nota">Formar profesionistas capaces de desarrollar, integrar y administrar tecnologías de la información y comunicaciones que contribuyan a la productividad y al logro de los objetivos estratégicos de las organizaciones en un entorno globalizado; caracterizándose por ser líderes, críticos, competentes, éticos y con visión emprendedora, comprometidos con el desarrollo sustentable.</span> <br>
+                <br><br>
+                <p class="itlcuerpo"> <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Perfil-Objetivo Ingenieria en Tecnologias de la Informacion y Comunicaciones.pdf" target="_blank">Perfil de Egreso</a></p>
+
+                <br>
+                <span class="nota">
+                    1. Diseña, implementa y administra redes de cómputo y comunicaciones para satisfacer las necesidades de información de las organizaciones, con base en modelos y estándares internacionales.
+                    <br>
+                    2. Administra proyectos que involucren Tecnologías de la Información y Comunicaciones para el
+                    logro de los objetivos organizacionales conforme a requerimientos establecidos.
+                    <br>
+                    3. Desarrolla e implementa sistemas de información para la gestión de procesos y apoyo en la toma de decisiones, utilizando metodologías basadas en estándares internacionales.
+                    <br>
+                    4. Diseña, desarrolla y gestiona sistemas de bases de datos para garantizar la integridad,
+                    disponibilidad y confidencialidad de la información.
+                    <br>
+                    5. Integra soluciones de sistemas de comunicación con diferentes tecnologías, plataformas o
+                    dispositivos.
+                    <br>
+                    6. Desempeña funciones de consultoría y auditoría para validar procesos y garantizar la calidad en el uso de las Tecnologías de la Información y Comunicaciones.
+                    <br>
+                    7. Crea empresas en el ámbito de las Tecnologías de la Información y Comunicaciones para contribuir
+                    al desarrollo del entorno.
+                    <br>
+                    8. Integra las diferentes arquitecturas de hardware y administra plataformas de software para
+                    incrementar la productividad en las organizaciones.
+                    <br>
+                    9. Implementa sistemas de seguridad acorde a políticas internas de las organizaciones basados en
+                    estándares establecidos, con la finalidad de garantizar la integridad y consistencia de la
+                    información.
+                    <br>
+                    10. Aplica los aspectos de legislación informática para regular el uso y explotación de las Tecnologías de la Información y Comunicaciones.
+                    <br>
+                    11. Diseña e implementa dispositivos con software embebido para aplicaciones de propósito específico.
+                    <br>
+                    12. Utiliza tecnologías emergentes y herramientas actuales para atender necesidades acordes al entorno.
+                    <br>
+                    13. Diseña e implementa interfaces gráficas de usuario para facilitar la interacción entre el ser humano, los equipos y sistemas electrónicos.
+                    <br>
+                    14. Posee habilidades metodológicas de investigación que fortalezcan el desarrollo cultural, científico y tecnológico en el ámbito de sistemas computacionales y disciplinas afines.
+                    <br>
+                    15. Selecciona y aplica herramientas matemáticas para el modelado, diseño y desarrollo de tecnología computacional.
+                    <br>
+                    16. Desempeña sus actividades profesionales considerando los aspectos legales, éticos, sociales y de desarrollo sustentable.
+
+                </span>
+            </div>
+            <br><br>
+            <div class="reticula">
+            <p class="itlcuerpo"> <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Reticula Ingenieria en Tecnologias de la Informacion y Comunicaciones.pdf" target="_blank">Retícula</a></p>
+
+            <p class="itlcuerpo"> <a href="../Programas/Ing en Tecnologias de la Informacion  ITIC - 2010 - 225/Retícula ITIC-2010-225 Mod Esp.pdf" target="_blank">ESPECIALIDAD <br>
+                </a></p>
+            <span class="nota">
+                Tecnologías Emergentes y Computo Móvil <br> ITIE-TEC-2018-01
+            </span>
+            </div>
+        </div>
+    </div>
+
+    <!--    <tr>  
+          <td colspan="2"><img src="img/lineahorizontal.png" alt="" width="680" height="29"></td>
+        </tr>
+        <tr>
+          <td width="267">&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>   --->
+</div>
