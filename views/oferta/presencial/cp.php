7dad4baa9781bb84008816bf4dab80ea7c3fa49b
Manzanarez <mamanzasanchez@gmail.com>
2021-05-28 14:59:19 -0500

    Se agrego la informacion a el archivo cp.php

--- views/oferta/presencial/cp.php
+++ views/oferta/presencial/cp.php
@@ -1,1 +1,216 @@
-<h2>cp</h2>
+<div align="left" class="Estilo4 centrado col-xs-12">
+    <h2 href="left" class="titulocarrera">Contador Público </h2>
+    <p class="info">COPU-2010-205</p>
+    <img src="../img/logos/LogoCP.jpg" width="110" height="120">
+    <!---- <img src="img/logos/LogoSistemas.jpg" align="left" width="50" height="60"> --->
+    <hr class="divisor">
+</div>
+<div class="col-xs-12 centrado">
+    <!------- Tabla principal ------>
+    <div class="centrado cuerpo">
+        <div class="menu_materias">
+            <!---COLUMNAS DE ASIGNATURAS ---->
+            <a href="#" class="itlcuerpo"> Asignaturas:&nbsp;</a>
+            <!---  <div class="cintillo"></div> --->
+            <!-------------Semestre 1 ----------------------------------->
+            <br>
+            <a href="#" class="titulonota">Semestre 1: </a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/INTRODUCCION A LA CONTABILIDAD FINANCIERA_OK.pdf" target="_blank">Introducción a la Contabilidad Financiera</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/ADMINISTRACION_OK.pdf" target="_blank">Administración</a>
+            <br>
+            <a href="../Programas/Asignaturas Comunes/AC003 Algebra Lineal.pdf" target="_blank">Álgebra Lineal</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/FUNDAMENTOS DE DERECHO_OK.pdf" target="_blank">Fundamentos de Derecho</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/DESARROLLO HUMANO_OK.pdf" target="_blank">Desarrollo Humano</a>
+            <br>
+            <a href="../Programas/Asignaturas Comunes/AC006 Fundamentos de Investigacion.pdf" target="_blank">Fundamentos de Investigacion</a>
+            <br>
+
+            <!------------------- semestre 2 ---------------->
+            <br>
+            <a href="#" class="titulonota">Semestre 2: </a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/CONTABILIDAD FINANCIERA I_OK.pdf" target="_blank">Contabilidad Financiera I</a>
+            <br>
+            <a href="../Programas/Asignaturas Comunes/AC007 Taller de Etica.pdf" target="_blank">Taller de Etica</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/CALCULO DIFERENCIA E INTEGRAL_OK.pdf" target="_blank">Cálculo Diferencial e Integral</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/DERECHO MERCANTIL_OK.pdf" target="_blank">Derecho Mercantil</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/DINAMICA SOCIAL_OK.pdf" target="_blank">Dinámica Social</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/ESTADISTICA ADMINISTRATIVA I_OK.pdf" target="_blank">Estadística Administrativa I</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/COMUINICACIÓN HUMANA_OK.pdf" target="_blank">Comunicación Humana</a>
+            <br>
+            <!------------------- semestre 3 ---------------->
+            <br>
+            <a href="#" class="titulonota">Semestre 3: </a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/CONTABILIDAD FINANCIERA II_OK.pdf" target="_blank">Contabilidad Financiera II</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/MERCADOTECNIA_OK.pdf" target="_blank">Mercadotecnia</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/MATEMATICAS FINANCIERAS_OK.pdf" target="_blank">Matemáticas Financieras</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/DERECHO LABORAL Y SEGURIDAD SOCIAL_OK.pdf" target="_blank">Derecho Laboral y Seguridad Social</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/GESTION DEL TALENTO HUMANO_OK.pdf" target="_blank">Gestión del Talento Humano</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/ESTADISTICA ADMINISTRATIVA II_OK.pdf" target="_blank">Estadística Administrativa II</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/TALLER DE INFORMATICA I_OK.pdf" target="_blank">Taller de Informática I</a>
+            <br>
+            <!------------------- semestre 4 ---------------->
+            <br>
+            <a href="#" class="titulonota">Semestre 4: </a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/CONTABILIDAD DE SOCIEDADES_OK.pdf" target="_blank">Contabilidad de Sociedades</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/SISTEMA DE COSTOS HISTORICOS_OK.pdf" target="_blank">Sistemas de Costos Históricos</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/MICROECONOMIA_OK.pdf" target="_blank">Microeconomía</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/DERECHO TRIBUTARIO_OK.pdf" target="_blank">Derecho Tributario</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/ANALISIS E INTERPRETACION DE ESTADOS FINANCIEROS_OK.pdf" target="_blank">Análisis e Interpretación de Estados Financieros</a>
+            <br>
+            <a href="../Programas/Asignaturas Comunes/AC009 Taller de Investigacion I.pdf" target="_blank">Taller de Investigación I</a>
+            <br>
+            <a href="../Programas/Asignaturas Comunes/AC008 Desarrollo Sustentable.pdf" target="_blank">Desarrollo Sustentable</a>
+            <br>
+
+            <!------------------- semestre 5 ---------------->
+            <br>
+            <a href="#" class="titulonota">Semestre 5: </a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/CONTABILIDAD AVANZADA_OK.pdf" target="_blank">Contabilidad Avanzada</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/SISTEMAS DE COSTOS PREDETERMINADOS_OK.pdf" target="_blank">Sistemas de Costos Predeterminados</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/MACROECONOMIA_OK.pdf" target="_blank">Macroeconomía</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/IMPUESTOS PERSONAS MORALES_OK.pdf" target="_blank">Impuestos Personas Morales</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/FUNDAMENTOS DE AUDITORIA_OK.pdf" target="_blank">Fundamentos de Auditoría</a>
+            <br>
+            <a href="../Programas/Asignaturas Comunes/AC010 Taller de Investigacion II.pdf" target="_blank">Taller de Investigación II</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/ADMINISTRACION DE LA PRODUCCION Y LAS OPERACIONES_OK.pdf" target="_blank">Admnistración de la Producción y de las Operaciones</a>
+            <br>
+            <!------------------- semestre 6 ---------------->
+            <br>
+            <a href="#" class="titulonota">Semestre 6: </a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/CONTABILIDAD INTERNACIONAL_OK.pdf" target="_blank">Contabilidad Internacional</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/GESTION Y TOMA DE DECISIONES_OK.pdf" target="_blank">Gestión y Toma de Decisiones</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/ADMINISTRACION ESTRATÉGICA_OK.pdf" target="_blank">Administración Estratégica</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/IMPUESTOS PERSONAS FISICAS_OK.pdf" target="_blank">Impuestos Personas Físicas</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/AUDITORIA PARA EFECTOS FINANCIEROS_OK.pdf" target="_blank">Auditoría para Efectos Financieros</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/ECONOMIA INTERNACIONAL_OK.pdf" target="_blank">Economía Internacional</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/PLANEACION FINANCIERA_OK.pdf" target="_blank">Planeación Financiera</a>
+            <br>
+
+            <!------------------- semestre 7 ---------------->
+            <br>
+            <a href="#" class="titulonota">Semestre 7: </a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/SEMINARIO DE CONTADURIA_OK.pdf" target="_blank">Seminario de Contaduría</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/ELABORACION Y EVAL DE PROY DE  NVERSION_OK.pdf" target="_blank">Elaboración y Eval. de Proy. de Inversión</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/OTROS IMPUESTOS Y CONTRIBUCIONES _OK.pdf" target="_blank">Otros Impuestos y Contribuciones</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/AUDITORIA PARA EFECTOS FISCALES_OK.pdf" target="_blank">Auditoría para efectos fiscales</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/TALLER DE INFORMATICA II_OK.pdf" target="_blank">Taller de Informática II</a>
+            <br>
+            <a href="../Programas/Contador Publico COPU - 2010 - 205/Asignaturas/ALTERNATIVAS DE INVERSION Y FINANCIAMIENTO_OK.pdf" target="_blank">Alternativas de Inversión y Financiamiento</a>
+            <br>
+
+            <!------------------- FINALIZA LA TABLA DE ASIGNATURAS ---------------->
+        </div>
+        <!---COLUMNAS DE PERFILIL Y RETICULAS ---->
+        <div class="descripcion_carrera">
+            <div class="b_lateral">
+                <br><br>
+                <p class="itlcuerpo"><a href="../Programas/Contador Publico COPU - 2010 - 205/Perfil-Objetivo Contador Publico.pdf" target="_blank">OBJETIVO GENERAL&nbsp;</a></p>
+
+                <span class="nota">Formar profesionales competitivos, capaces de diseñar, establecer, aplicar, controlar y evaluar sistemas de información financiera, administrativa y fiscal, dentro del marco legal vigente para la toma de decisiones de las entidades económicas nacionales e internacionales, con una actitud ética, crítica, emprendedora y de liderazgo, a través de la investigación y el uso de la tecnología de la información y comunicaciones, fomentando el desarrollo sustentable.</span> <br>
+                <br><br>
+                <p class="itlcuerpo"> <a href="#">MISIÓN</a></p>
+
+                <span class="nota">Formar personas integras en el área de la contaduría pública, competitivas nacional e internacionalmente en su campo profesional, que al mismo tiempo sea ciudadanos comprometidos con el desarrollo económico, político, social, cultural y sustentable con sentido de responsabilidad y ética consigo mismo y su entorno. </span> <br>
+                <br><br>
+                <p class="itlcuerpo"> <a href="#">VISIÓN</a> </p>
+                <span class="nota"> Ser de los mejores institutos que ofrece la carrera de Contador Público reconocido a nivel nacional e internacional por su compromiso permanente con la excelencia en la formación de profesionales con valores solidarios e institucionales que contribuyen al desarrollo sustentable de su entorno.
+                </span><br><br><br>
+                <p class="itlcuerpo"><a href="#">PERFIL DE INGRESO&nbsp; </a></p>
+                <span class="nota">
+                    <ul>
+                        <li>El aspirante de la carrera de Contador Público debe poseer las siguientes características:</li>
+                        <li> Manejo de las tecnologías de la información y comunicación.</li>
+                        <li> Habilidad para el trabajo en equipo. </li>
+                        <li> Creatividad </li>
+                        <li>Solucionador de retos.</li>
+                    </ul>
+                </span>
+                <br><br>
+                <p class="itlcuerpo"> <a href="../Programas/Contador Publico COPU - 2010 - 205/Perfil-Objetivo Contador Publico.pdf" target="_blank">PERFIL DE EGRESO&nbsp;</a></p>
+
+                <span class="nota">
+                    1. Diseña, implanta, controla, evalúa, asesorara e innova sistemas de información financiera,
+                    administrativa, fiscal y de auditoría en entidades económicas para la generación, análisis de
+                    información financiera con apego a las Normas de Información Financiera, nacionales e
+                    internacionales y la toma de decisiones.
+                    <br>
+                    2. Audita sistemas financieros, fiscales y administrativos de las entidades económicas con apego a las Normas Internacionales de Auditoria vigentes para la emisión de un dictamen.
+                    <br>
+                    3. Aplica el marco legal y pertinente a las características y necesidades de la entidad económica
+                    dentro del campo profesional para el cumplimiento de las disposiciones vigentes.
+                    <br>
+                    4. Administra estratégicamente los recursos de las entidades económicas para el cumplimiento de sus
+                    objetivos.
+                    <br>
+                    5. Aplica en el ejercicio profesional el código de ética del contador público vigente para enaltecer a la profesión.
+                    <br>
+                    6. Utiliza las tecnologías de información y comunicación para eficientar los procesos y la toma de
+                    decisiones.
+                    <br>
+                    7. Desarrolla investigación asumiendo una actitud de liderazgo, compromiso y servicio con su entorno social, para su difusión.
+                    <br>
+                    8. Elabora y evalúa proyectos de inversión de acuerdo a las características y necesidades del entorno para la generación de entidades económicas con actitud de liderazgo y compromiso con el medio ambiente.
+                    <br>
+                    9. Trabaja en equipos multidisciplinarios para el logro de los resultados de las entidades con un
+                    sentido de responsabilidad social y visión integradora.
+                    <br>
+                    10. Propone estrategias de mercadotecnia que permitan alcanzar los objetivos de las entidades
+                    económicas.
+                    <br>
+                    11. Aplica métodos de análisis de información financiera para determinar las mejores alternativas de inversión y financiamiento.
+                </span>
+            </div>
+            <br><br>
+            <div class="reticula">
+                <p class="itlcuerpo"> <a href="../Programas/Contador Publico COPU - 2010 - 205/Reticula Contador .pdf" target="_blank">RETÍCULA
+                </p>
+
+                <br>
+                <p class="itlcuerpo"><a href="../Programas/Contador Publico COPU - 2010 - 205/Reticula-espContador Publico.pdf" target="_blank">ESPECIALIDAD</a> </p>
+                <span class="nota"> <br>
+                    Finanzas (COPUFIN 201801)
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
