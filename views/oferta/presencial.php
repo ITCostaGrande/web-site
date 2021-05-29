d2160cadee266bdeb02411fc25646e948dbcb3d3
Manzanarez <mamanzasanchez@gmail.com>
2021-05-28 19:23:36 -0500

    Se modifico el archivo ige.php

--- views/oferta/presencial.php
+++ views/oferta/presencial.php
@@ -10,9 +10,9 @@
                 <li><a href="/oferta/cp"><h4>CONTADOR PÚBLICO</h4></a></li>
                 <li><a href="/oferta/admon"><h4>LICENCIATURA EN ADMINISTRACIÓN</h4></a></li>
                 <li><a href="/oferta/tics"><h4>INGENIERÍA EN TIC'S</h4></a></li>
-                <li><a href="/oferta/isc"><h4>INGENIERÍA EN GESTIÓN EMPRESARIAL</h4></a></li>
-                <li><a href="/oferta/electromecanica"><h4>INGENIERÍA EN SISTEMAS COMPUTACIONALES</h4></a></li>
-                <li><a href="/oferta/ige"><h4>INGENIERÍA ELECTROMECÁNICA</h4></a></li>
+                <li><a href="/oferta/ige"><h4>INGENIERÍA EN GESTIÓN EMPRESARIAL</h4></a></li>
+                <li><a href="/oferta/isc"><h4>INGENIERÍA EN SISTEMAS COMPUTACIONALES</h4></a></li>
+                <li><a href="/oferta/electromecanica"><h4>INGENIERÍA ELECTROMECÁNICA</h4></a></li>
             </ul>
         </div>
     </div>
--- views/oferta/presencial/ige.php
+++ views/oferta/presencial/ige.php
@@ -1,1 +1,259 @@
-<h2>ige</h2>
+<div align="left" class="Estilo4 centrado col-xs-12">
+    <h2 href="left" class="titulocarrera">Ingeniería en Gestión Empresarial</h2>
+
+    <img src="../img/logos/LogoIGE.jpg" width="110" height="120">
+    <!---- <img src="img/logos/LogoSistemas.jpg" align="left" width="50" height="60"> --->
+    <hr class="divisor">
+</div>
+
+
+<!------- Tabla principal ------>
+<div class="col-xs-12 centrado">
+    <div class="centrado cuerpo">
+        <!---COLUMNAS DE ASIGNATURAS ---->
+        <div class="menu_materias">
+            <p><a href="#" class="itlcuerpo"> Asignaturas:&nbsp;</a>
+                <!---  <div class="cintillo"></div> --->
+                <!-------------Semestre 1 ----------------------------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 1: </a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC006 Fundamentos de Investigacion.pdf" target="_blank">Fundamentos de Investigación</a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC001 Calculo Diferencial.pdf" target="_blank">Cálculo Diferencial</a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Desarrollo Humano.pdf" target="_blank">Desarrollo Humano</a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE074 Fundamentos de Gestion Empresarial.pdf" target="_blank">Fundamentos de Gestión Empresarial</a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Fundamentos de Fisica.pdf" target="_blank">Fundamentos de Física</a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Fundamentos de Química.pdf" target="_blank">Fundamentos de Química</a>
+                <br>
+
+                <!------------------- semestre 2 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 2: </a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE082 Software de Aplicacion Ejecutivo.pdf" target="_blank">Software de Aplicación Ejecutivo</a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC002 Calculo Integral.pdf" target="_blank">Cálculo Integral</a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Contabilidad orientada a los Negocios.pdf" target="_blank">Contabilidad orientada a los Negocios</a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE014 Dinamica Social.pdf" target="_blank">Dinámica Social</a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC007 Taller de Etica.pdf" target="_blank">Taller de Ética</a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Legislación Laboral.pdf" target="_blank">Legislación Laboral</a>
+                <br>
+                <!------------------- semestre 3 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 3: </a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE078 Marco Legal de las Organizaciones.pdf" target="_blank">Marco Legal de las Organizaciones</a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Probabilidad y Estadística Descriptiva.pdf" target="_blank">Probabilidad y Estadística Descriptiva</a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Costos Empresariales.pdf" target="_blank">Costos Empresariales</a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Habilidades Directivas I.pdf" target="_blank">Habilidades Directivas I</a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE071 Economia Empresarial.pdf" target="_blank">Economia Empresarial</a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC003 Algebra Lineal.pdf" target="_blank">Álgebra Lineal</a>
+                <br>
+                <!------------------- semestre 4 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 4: </a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Ingeniería Económica.pdf" target="_blank">Ingeniería Económica</a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Estadística Inferencial I.pdf" target="_blank">Estadística Inferencial I</a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Instrumentos de Presupuestación Empresarial.pdf" target="_blank">Instrumentos de Presupuestación Empresarial</a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Habilidades Directivas II.pdf" target="_blank">Habilidades Directivas II</a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Entorno Macroecónomico.pdf" target="_blank">Entorno Macroecónomico</a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE076 Investigacion de Operaciones.pdf" target="_blank">Investigación de Operaciones</a>
+                <br>
+
+                <!------------------- semestre 5 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 5: </a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE073 Finanzas de las Organizaciones.pdf" target="_blank">Finanzas de las Organizaciones</a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Estadistica Inferencial II.pdf" target="_blank">Estadistica Inferencial II</a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Ingeniería de Procesos.pdf" target="_blank">Ingeniería de Procesos</a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE075 Gestion del Capital Humano.pdf" target="_blank">Gestion del Capital Humano</a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC009 Taller de Investigacion I.pdf" target="_blank">Taller de Investigacion I</a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Mercadotecnia.pdf" target="_blank">Mercadotecnia</a>
+                <br>
+                <!------------------- semestre 6 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 6: </a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Administración de la Salud y Seguridad Ocupacional.pdf" target="_blank">Administración de la Salud y Seguridad Ocupacional</a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE072 El emprendedor y la Innovacion.pdf" target="_blank">El emprendedor y la Innovacion</a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Gestion de la Producción I.pdf" target="_blank">Gestión de la Producción I</a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE015 Diseño Organizacional.pdf" target="_blank">Diseño Organizacional</a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC010 Taller de Investigacion II.pdf" target="_blank">Taller de Investigacion II</a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Sistemas de Información de Mercadotecnia.pdf" target="_blank">Sistemas de Información de Mercadotecnia</a>
+                <br>
+
+                <!------------------- semestre 7 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 7: </a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE069 Calidad Aplicada a la Gestion Empresarial.pdf" target="_blank">Calidad Aplicada a la Gestion Empresarial</a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Plan de Negocios.pdf" target="_blank">Plan de Negocios</a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Gestion de la Produccion II.pdf" target="_blank">Gestión de la Producción II</a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/GestionEstrategica.pdf" target="_blank">Gestión Estratégica</a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC008 Desarrollo Sustentable.pdf">Desarrollo Sustentable</a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE045 Mercadotecnia Electronica.pdf">Mercadotecnia Electrónica</a>
+                <br>
+
+                <!------------------- semestre 8 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 8: </a>
+                <br>
+                <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/asignaturas/Cadena de Sumunistros.pdf" target="_blank">Cadena de Sumunistros</a>
+                <hr class="divisor">
+            </p>
+            <p class="itlcuerpo"> <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/Reticula Ingenieria en Gestion Empresarial.pdf" target="_blank">RetÍcula</a></p>
+            <p class="itlcuerpo"><a href="../archivos/InfoIGE/reticula_con_especialidad.pdf" target="_blank">Especialidad</a></p>
+
+            <span class="nota"> Logística IGEE-LOG-2018-01 </span>
+            <p class="nota"> <a href="../archivos/material/Programa_de_asesorías/Asesoría2018-2.pdf"> Programa de Asesorías</a>:</p>
+            <!--  <p><a href="archivos/InfoIGE/Programa de esesorías/Programa de asesorias Ciencias Básicas AD17.pdf" target="_blank"><img src="img/pdf.jpg" width="18" height="19"> CIENCIAS BÁSICAS </a>            </p> -->
+            <p class="nota"> <a href="../archivos/InfoIGE/Programa de esesorías/Tutoria.pdf" >Programa de Tutorías </a> </p>
+
+        </div>
+        <!------------------- FINALIZA LA TABLA DE ASIGNATURAS ---------------->
+
+        <!---COLUMNAS DE PERFILIL Y RETICULAS ---->
+        <div class="descripcion_carrera">
+            <div class="b_lateral">
+                <p class="itlcuerpo"><a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/Perfil-Objetivo Ingenieria en Gestion Empresarial.pdf" target="_blank">Objetivo General</a></p>
+                <br>
+                <span class="nota">Formar profesionales que contribuyan a la gestión de empresas e innovación de procesos; así como al diseño, implementación y desarrollo de sistemas estratégicos de negocios, optimizando recursos en un entorno global, con ética y responsabilidad social.</span> <br>
+                <br><br>
+                <p class="itlcuerpo"> <a href="../Programas/Ing en Gestion Empresarial IGEM - 2009 - 201/Perfil-Objetivo Ingenieria en Gestion Empresarial.pdf" target="_blank">Perfil de Egreso</a></p>
+
+                <p><br>
+                    <span class="nota">
+                        1. Aplica habilidades directivas y de ingeniería en el diseño, gestión, fortalecimiento e innovación de las organizaciones para la toma de decisiones en forma efectiva, con una orientación sistémica y sustentable.
+                        <br>
+                        2. Diseña e innova estructuras administrativas y procesos, con base en las necesidades de las
+                        organizaciones para competir eficientemente en mercados globales.
+                        <br>
+                        3. Gestiona eficientemente los recursos de la organización con visión compartida, con el fin de
+                        suministrar bienes y servicios de calidad.
+                        <br>
+                        4. Aplica métodos cuantitativos y cualitativos en el análisis e interpretación de datos y modelado de sistemas en los procesos organizacionales, para la mejora continua atendiendo estándares de calidad mundial.
+                        <br>
+                        5. Diseña, y emprende nuevos negocios y proyectos empresariales sustentables en mercados
+                        competitivos, para promover el desarrollo.
+                        <br>
+                        6. Diseña e implementa estrategias de mercadotecnia basadas en información recopilada de fuentes
+                        primarias y secundarias, para incrementar la competitividad de las organizaciones.
+                        <br>
+                        7. Implementa planes y programas de seguridad e higiene para el fortalecimiento del entorno laboral.
+                        <br>
+                        8. Gestiona sistemas integrales de calidad para la mejora de los procesos, ejerciendo un liderazgo
+                        estratégico y un compromiso ético.
+                        <br>
+                        9. Aplica las normas legales para la creación y desarrollo de las organizaciones.
+                        <br>
+                        10. Dirige equipos de trabajo para la mejora continua y el crecimiento integral de las organizaciones.
+                        <br>
+                        11. Interpreta la información financiera para detectar oportunidades de mejora e inversión en un mundo global, que propicien la rentabilidad del negocio.
+                        <br>
+                        12. Utiliza las nuevas tecnologías de información y comunicación en la organización, para optimizar
+                        los procesos y la eficaz toma de decisiones.
+                        <br>
+                        13. Promueve el desarrollo del capital humano, para la realización de los objetivos organizacionales, dentro de un marco ético y un contexto multicultural.
+                        <br>
+                        14. Aplica métodos de investigación para desarrollar e innovar modelos, sistemas, procesos y productos en las diferentes dimensiones de la organización.
+                        <br>
+                        15. Gestiona la cadena de suministro de las organizaciones con un enfoque orientado a procesos para
+                        incrementar la productividad.
+                        <br>
+                        16. Analiza las variables económicas para facilitar la toma estratégica de decisiones en la organización.
+                        <br>
+                        17. Actúa como agente de cambio para facilitar la mejora continua y el desempeño de las
+                        organizaciones.
+                        <br>
+                        18. Aplica métodos, técnicas y herramientas para la solución de problemas en la gestión empresarial
+                        con una visión estratégica. </span>
+                </p>
+
+                <p class="itlcuerpo"><a href="">Perfil de Ingreso</a> </p>
+                <br>
+                <span class="nota"> Los aspirantes a estudiar éste programa deberán tener atracción e inclinación hacia las ciencias exactas, una de las características que sobresalen son las siguientes: <br>
+                    • Habilidad analítica. <br>
+                    • Interés por aprender. <br>
+                    • Matemáticas básicas.</span>
+                <p>
+                <p class="itlcuerpo"><a href="../archivos/material/Expo_2016/MEMORIADELEVENTO.pdf"> Expo proyectos: </a></p>
+                <br>
+                <span class="nota">
+                    Éste evento académico, es organizado por estudiantes y docentes de las carreras de Ingeniería Electromecánica e Ingeniería en Gestión Empresarial en donde se presentan los proyectos finales de las materias que están cursando como parte de las competencias que tienen que desarrollar, en la transferencia del conocimiento los estudiantes explican sus proyectos a visitantes de escuelas de nivel primaria, secundaria y bachillerato del municipio de Zihuatanejo de Azueta.
+                </span>
+                <br>
+                <br>
+            </div>
+            <div class="centrado recuadro">
+                    <p class="itlcuerpo">Banco de proyectos para Residencia Profesional:</p>
+                    <br>
+                    <span class="nota">
+                        • Sistemas de Gestión de Calidad <br>
+                        • Sistemas de Gestión Ambiental <br>
+                        • Sistemas de Gestión de Salud ocupacional
+                    </span>
+                    <p>&nbsp;</p>
+                    <p>&nbsp;</p>
+                </div>
+                <br><br>
+            <p class="itlcuerpo">Contacto: </p>
+            <div align="center"><br>
+                <span class="pie">Ing. Andrés Amparán Ocampo, <br>
+                    Jefe del Departamento de Metal Mecánica.<br>
+                    Teléfono: 755 554-48-52; Ext 303<br>
+                    Email: <a href="mailto:itcg_metalmecanica@outlook.com">itcg_metalmecanica@outlook.com</a><br>
+                    Facebook: <a href="https://www.facebook.com/TecNM.ITCG?ref=br_rs" target="_blank">Metalmecánica Itcg</a><br>
+                    Edificio G, Laboratorio de Electromecánica.<br>
+                </span>
+               
+            </div>&nbsp; </p>
+        </div>
+    </div>
+
+    <!--    <tr>  
+          <td colspan="2"><img src="img/lineahorizontal.png" alt="" width="680" height="29"></td>
+        </tr>
+        <tr>
+          <td width="267">&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>   --->
+</div>
+
+
+<!------- Finaliza Tabla principal ------>
