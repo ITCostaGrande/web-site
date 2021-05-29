423cafec24c0b7b4fd04c87dcc310898a30d491b
Manzanarez <mamanzasanchez@gmail.com>
2021-05-28 20:10:19 -0500

    Se modifico el archivo isc.php

--- views/oferta/presencial/isc.php
+++ views/oferta/presencial/isc.php
@@ -1,1 +1,216 @@
-<h2>isc</h2>
+<div align="left" class="Estilo4 centrado col-xs-12">
+    <h2>Ingeniería en Sistemas Computacionales</h2>
+
+    <img src="../img/logos/LogoSistemas.jpg" align="center" width="110" height="120">
+    <hr class="divisor">
+
+</div>
+<!------- Tabla principal ------>
+<table width="680" border="0" cellspacing="0" cellpadding="0" align="center">
+    <tr>
+        <!---COLUMNAS DE ASIGNATURAS ---->
+        <td width="250">
+            <p><a href="#" class="itlcuerpo"> Asignaturas:&nbsp;</a>
+                <!---  <div class="cintillo"></div> --->
+                <!-------------Semestre 1 ----------------------------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 1: </a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC001 Calculo Diferencial.pdf" target="_blank">Cálculo Diferencial</a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE032 Fundamentos de Programacion.pdf" target="_blank">Fundamentos de Programación</a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC007 Taller de Etica.pdf" target="_blank">Taller de ética</a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE041 Matematicas Discretas.pdf" target="_blank">Matemáticas Discretas</a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Taller de Administración.pdf" target="_blank">Taller de Administración</a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC006 Fundamentos de Investigacion.pdf" target="_blank">Fundamentos de Investigación</a>
+                <br>
+
+                <!------------------- semestre 2 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 2: </a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC002 Calculo Integral.pdf" target="_blank">Cálculo Integral</a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE054 Programacion Orientada a Objetos.pdf" target="_blank">Programación Orientada a Objetos</a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE008 Contabilidad Financiera.pdf" target="_blank">Contabilidad Financiera</a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE058 Quimica.pdf" target="_blank">Química</a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC003 Algebra Lineal.pdf" target="_blank">Álgebra Líneal</a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE052 Probabilidad y Estadistica.pdf" target="_blank">Probabilidad y Estadística</a>
+                <br>
+                <!------------------- semestre 3 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 3: </a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC004 Calculo Vectorial.pdf" target="_blank">Cálculo Vectorial</a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE026 Estructura de Datos.pdf" target="_blank">Estructura de Datos</a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Cultura Empresarial.pdf" target="_blank">Cultura Empresarial</a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Investigación de operaciones.pdf" target="_blank">Investigación de operaciones</a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC008 Desarrollo Sustentable.pdf" target="_blank">Desarrollo Sustentable</a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Física General.pdf" target="_blank">Física General</a>
+                <br>
+                <!------------------- semestre 4 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 4: </a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC005 Ecuaciones Diferenciales.pdf" target="_blank">Ecuaciones Diferenciales</a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Métodos numéricos.pdf" target="_blank">Métodos numéricos</a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Tópicos Avanzados de Programación..pdf" target="_blank">Tópicos Avanzados de Programación</a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE031 Fundamentos de Base de Datos.pdf" target="_blank">Fundamentos de Base de Datos</a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Simulación.pdf" target="_blank">Simulación</a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Principios Eléctricos y Aplicaciones Digitales.pdf" target="_blank">Principios Eléctricos y Aplicaciones Digitales</a>
+                <br>
+
+                <!------------------- semestre 5 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 5: </a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Graficación.pdf" target="_blank">Graficación</a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE034 Fundamentos de Telecomunicaciones.pdf" target="_blank">Fundamentos de Telecomunicaciones</a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE061 Sistemas Operativos I.pdf" target="_blank">Sistemas Operativos</a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE063 Taller de Base de Datos.pdf" target="_blank">Taller de Base de Datos</a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Fundamentos de Ingeniería de Software.pdf" target="_blank">Fundamentos de Ingeniería de Software</a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Arquitectura de Computadoras..pdf" target="_blank">Arquitectura de Computadoras</a>
+                <br>
+                <!------------------- semestre 6 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 6: </a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Lenguajes y Autómatas I.pdf" target="_blank">Lenguajes y Autómatas I</a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Redes de Computadoras.pdf" target="_blank">Redes de Computadoras</a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Taller de Sistemas Operativos.pdf" target="_blank">Taller de Sistemas Operativos</a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Administración de Base de Datos.pdf" target="_blank">Administración de Base de Datos</a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Ingeniería de Software.pdf" target="_blank">Ingeniería de Software</a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Lenguajes de Interfaz.pdf" target="_blank">Lenguajes de Interfaz</a>
+                <br>
+
+                <!------------------- semestre 7 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 7: </a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Lenguajes y Autómatas II..pdf" target="_blank">Lenguajes y Autómatas II</a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Conmutación y Enrutamiento en Redes de Datos.pdf" target="_blank">Conmutación y Enrutamiento en Redes de Datos</a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC009 Taller de Investigacion I.pdf" target="_blank">Taller de Investigación I</a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Gestión de Proyectos de Software.pdf" target="_blank">Gestión de Proyectos de Software</a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Sistemas Programables.pdf" target="_blank">Sistemas Programables</a>
+                <br>
+
+                <!------------------- semestre 8 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 8: </a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Programación Lógica y Funcional..pdf" target="_blank">Programación Lógica y Funcional</a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Administración de redes..pdf" target="_blank">Administración de redes</a>
+                <br>
+                <a href="../Programas/Asignaturas Comunes/AC010 Taller de Investigacion II.pdf" target="_blank">Taller de Investigación II</a>
+                <br>
+                <a href="../Programas/Asignaturas Equivalentes/AE055 Programacion Web.pdf" target="_blank">Programación Web</a>
+                <br>
+                <!---------- semestre 9 ---------------->
+                <br>
+                <a href="#" class="titulonota">Semestre 9: </a>
+                <br>
+                <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Asignaturas/Inteligencia Artificial..pdf" target="_blank">Inteligencia Artificial</a>
+
+            </p>
+            <p>&nbsp;</p>
+
+        </td>
+        <!------------------- FINALIZA LA TABLA DE ASIGNATURAS ---------------->
+
+        <!---COLUMNAS DE PERFILIL Y RETICULAS ---->
+        <td width="415" valign="top" align="justify">
+            <div class="b_lateral">
+                <p class="itlcuerpo"><a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Perfil-Objetivo Ingenieria en Sistemas Computacionales.pdf" target="_blank">Objetivo General&nbsp;</a></p>
+                <br>
+                <span class="nota">Formar profesionistas líderes con visión estratégica y amplio sentido ético; capaz de diseñar, desarrollar, implementar y administrar tecnología computacional para aportar soluciones innovadoras
+                    en beneficio de la sociedad; en un contexto global, multidisciplinario y sostenible.</span> <br>
+                <br><br>
+                <p class="itlcuerpo"> <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Perfil-Objetivo Ingenieria en Sistemas Computacionales.pdf" target="_blank">Perfil de egreso&nbsp;</a></p>
+
+                <br>
+                <span class="nota">
+                    1. Implementa aplicaciones computacionales para solucionar problemas de diversos contextos,
+                    integrando diferentes tecnologías, plataformas o dispositivos.
+                    <br>
+                    2. Diseña, desarrolla y aplica modelos computacionales para solucionar problemas, mediante la
+                    selección y uso de herramientas matemáticas.
+                    <br>
+                    3. Diseña e implementa interfaces para la automatización de sistemas de hardware y desarrollo del
+                    software asociado.
+                    <br>
+                    4. Coordina y participa en equipos multidisciplinarios para la aplicación de soluciones innovadoras en diferentes contextos.
+                    <br>
+                    5. Diseña, implementa y administra bases de datos optimizando los recursos disponibles, conforme a
+                    las normas vigentes de manejo y seguridad de la información.
+                    <br>
+                    6. Desarrolla y administra software para apoyar la productividad y competitividad de las
+                    organizaciones cumpliendo con estándares de calidad.
+                    <br>
+                    7. Evalúa tecnologías de hardware para soportar aplicaciones de manera efectiva.
+                    <br>
+                    8. Detecta áreas de oportunidad empleando una visión empresarial para crear proyectos aplicando las
+                    Tecnologías de la Información y Comunicación.
+                    <br>
+                    9. Diseña, configura y administra redes de computadoras para crear soluciones de conectividad en la
+                    organización, aplicando las normas y estándares vigentes.</span>
+            </div>
+            <br><br>
+            <p class="itlcuerpo"> <a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Reticula Ingenieria en Sistemas Computacionales.pdf" target="_blank">Retícula</a> </p>
+            <br>
+            <p class="itlcuerpo"><a href="../Programas/Ing en Sistemas Computacionales ISIC - 2010 - 224/Atributos y objetivos educacionales ISC.pdf" target="_blank">Atributos y objetivos educacionales</a> </p>
+            <p>
+            
+            <p class="itlcuerpo"><a href="../archivos/infoISC/Asesorias.pdf">Programa de Asesorías:</a></p>
+            <!--  <p><a href="archivos/InfoIGE/Programa de esesorías/Programa de asesorias Ciencias Básicas AD17.pdf" target="_blank"><img src="img/pdf.jpg" width="18" height="19"> CIENCIAS BÁSICAS </a>            </p> -->
+            <p class="itlcuerpo"><a href="../archivos/infoISC/Asignación_tutores.pdf"> asignaciÓN DE TUTOREs</a> &nbsp; </p>
+        </td>
+    </tr>
+
+    <!--    <tr>  
+          <td colspan="2"><img src="img/lineahorizontal.png" alt="" width="680" height="29"></td>
+        </tr>
+        <tr>
+          <td width="267">&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>   --->
+</table>
+<!--- AQUá ACABA LOS BOLETINES ------>
+
+
+<!------- Finaliza Tabla principal ------>
+
+
+<!-- InstanceEndEditable -->
