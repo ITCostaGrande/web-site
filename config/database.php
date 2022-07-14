c8bab919f9d96257fc801fa59b7e8f2a70d42055
Julio C. Lopez <lopezjuliosanchez@hotmail.com>
2022-07-14 14:48:00 -0500

    se descomento base de datos

--- config/database.php
+++ config/database.php
@@ -1,14 +1,15 @@
 <?php
-/*
+
 function conectar(){
-    $conn = mysqli_connect('localhost','root','','itcos268_bdtec');
+    $conn = mysqli_connect('database-itcg.ca9wqlqc70ox.us-east-1.rds.amazonaws.com
+    ','admin','tecnmitcg','itcg');
     if(!$conn){
         echo 'No se pudo conectar';
         exit;
     }
 
     return $conn; 
-}*/
+}
 
     
     
